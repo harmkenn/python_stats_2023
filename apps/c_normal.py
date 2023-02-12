8a87374e343d24a9b5fd4bd59ac77efbfd0d234c
harmkenn <harmkenn@gmail.com>
2023-02-12 17:36:59 +0100

    Prob to z complete

--- apps/c_normal.py
+++ apps/c_normal.py
@@ -66,32 +66,25 @@ def app():
         
         g1,g2 = st.columns((1,3))
         with g2:
-            x = np.arange(-4,4,.1)
+            x = np.arange(-4,4,.01)
             y = norm.pdf(x)
             ndf = pd.DataFrame({"x":x,"y":y})
-            normp = ggplot(ndf) + geom_line(aes(x=x,y=y)) + coord_fixed(ratio = 4) 
+            fig = px.line(ndf, x = 'x', y = 'y', template= 'simple_white')
 
             if shade == "Left":
                 z = norm.ppf(sp/100)
-                normp = normp + stat_function(fun = norm.pdf, geom = "area",fill = "steelblue", xlim = (-4,z))
-                lz = z
-                rz = z
+                ndf.loc[(ndf.x >= z),'y'] = 0
+                                
             if shade == "Center":
                 z = norm.ppf(((100-sp)/2)/100)
-                lz = z 
-                rz = -z
-                normp = normp + stat_function(fun = norm.pdf, geom = "area",fill = "steelblue", xlim = (lz,rz))
-                
+                ndf.loc[(ndf.x <= z) | (ndf.x >= -z),'y'] = 0
+                                
             if shade == "Right":
                 z = norm.ppf((100-sp)/100)
-                lz = z
-                rz = z
-                normp = normp + stat_function(fun = norm.pdf, geom = "area",fill = "steelblue", xlim = (lz,4))
-                
-            normp = normp + geom_segment(aes(x = lz, y = 0, xend = lz, yend = norm.pdf(lz)),color="red")
-            normp = normp + geom_segment(aes(x = rz, y = 0, xend = rz, yend = norm.pdf(rz)),color="red")
-            normp = normp + geom_line(aes(x=x,y=y)) + coord_fixed(ratio = 4) + xlab('z') + ylab('')
-            st.pyplot(ggplot.draw(normp))
+                ndf.loc[(ndf.x <= z),'y'] = 0
+            fig.add_trace(px.area(ndf, x = 'x', y = 'y', template= 'simple_white').data[0])
+            st.plotly_chart(fig, use_container_width=True) 
+              
         with g1:
             st.markdown(f"z-Score: {z}")
             
